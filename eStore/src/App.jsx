bcdeb9efed2a577b9050fbe5deefff66ef7a24ab
Abhishek Pandey <abhishekpandey.codes@gmail.com>
2025-12-12 23:06:24 +0530

    Created structure

--- eStore/src/App.jsx
+++ eStore/src/App.jsx
@@ -1,12 +1,13 @@
-
+import { Outlet } from 'react-router-dom'
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
 
 
   return (
     <>
-     <h1 className='bg-amber-200'>Hello</h1>
+    <Navbar/>
+    <Outlet/>
     </>
   )
 }
